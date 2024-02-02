749ce6ce245785948b437464f468efbb583d6009
本木栗 Bemly_ <2328712329@qq.com>
2024-02-03 05:54:08 +0800

    测试下仓库里面的代码能不能绕过同源策略的影响

---
+++ push.js
@@ -0,0 +1,3 @@
+var gogogo = () => fetch("https://github.com/login/oauth/access_token?client_id=eaa7fedee07299f28436&client_secret=ea3e19035292dd20527f26819a9448f1f05c40b7&code=" + parameterValue)
+    .then(data => console.log(data))
+    .catch(err => console.error(err));
